70ac8487ccd4c0aa7038fcf8469d60d4fb4500f0
Mobin Akhtar Khavari <megamobin@gmail.com>
2025-02-25 09:07:05 -0300

    Bien hasta 102

--- 04-giff-expert-app/src/components/GifItem.jsx
+++ 04-giff-expert-app/src/components/GifItem.jsx
@@ -1,14 +1,10 @@
 export const GifItem = ({ title, id, url }) => {
-	// console.log(image);
-
-	// return <div>grid</div>;
 	return (
 		<div className='card'>
 			<img
 				src={url}
 				alt={title}
 			/>
-			<p>{title}</p>
 			<li key={id}>{title}</li>;
 		</div>
 	);
