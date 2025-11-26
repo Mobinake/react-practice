3a38cfad175cc374e813bd91dc0fa84ff1be04fd
Mobin <Megamobin@gamil.com>
2025-11-26 15:36:38 -0300

    Listo hasta cap 180

---
+++ 13-heroes-app/src/heroes/pages/hero/HeroPage.tsx
@@ -0,0 +1,10 @@
+import { Jumbotron } from '@/components/custom/Jumbotron';
+
+export const HeroPage = () => {
+	return (
+		<Jumbotron
+			title="Heroe!"
+			description="Descripcion de heroe"
+		/>
+	);
+};
---
+++ 13-heroes-app/src/heroes/pages/search/SearchPage.tsx
@@ -0,0 +1,14 @@
+import { Jumbotron } from '@/components/custom/Jumbotron';
+
+export const SearchPage = () => {
+	return (
+		<>
+			<Jumbotron
+				title="Buscando Heroes"
+				description="Descubre a los heroes."
+			/>
+		</>
+	);
+};
+
+export default SearchPage;
